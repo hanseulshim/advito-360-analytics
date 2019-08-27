bf45c897576bcd0ae492ea9660ba773c6ccd33b8
Hanseul Shim <hanseulshim@gmail.com>
2019-08-26 23:52:02 -0400

    Update url order

--- server/config.js
+++ server/config.js
@@ -1,11 +1,8 @@
 export const ADVITO_AIR_APPLICATION = 2;
 // export const APP_URL = 'http://localhost:8080/#'
-export const APP_URL =
-  'https://s3.amazonaws.com/alpha.boostlabs/air-dev/index.html?#';
-// export const APP_URL =
-//   'https://s3.amazonaws.com/beta.boostlabs/air/index.html#';
-// export const APP_URL =
-//   'https://s3.amazonaws.com/alpha.boostlabs/air/index.html#';
+// export const APP_URL = 'https://s3.amazonaws.com/alpha.boostlabs/air/index.html#'
+// export const APP_URL = 'https://s3.amazonaws.com/beta.boostlabs/air/index.html#'
+export const APP_URL = 'https://s3.amazonaws.com/alpha.boostlabs/air-dev/index.html?#'
 
 export const EMAIL_SENDER = 'IandA@advito.com';
 export const EMAIL_BCC = ['ianda@advito.com', 'hshim@boostlabs.com'];
