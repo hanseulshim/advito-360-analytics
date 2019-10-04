611ab8e6aa0ef4b601e568aa10a773ef2163d07c
Hanseul Shim <hanseulshim@gmail.com>
2019-10-04 15:40:16 -0400

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+function App () {
   return (
     <div>
       hello
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
