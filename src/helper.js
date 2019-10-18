28cfadc5a3290fc1da03bd21d58ba9ee3cd334ce
Hanseul Shim <hanseulshim@gmail.com>
2019-10-18 00:17:17 -0400

    Delete hardcoded session token

--- src/helper.js
+++ src/helper.js
@@ -9,8 +9,6 @@ export const getToken = () => {
   if (localStorage.getItem('advito-360-user')) {
     const { sessionToken } = JSON.parse(localStorage.getItem('advito-360-user'))
     return sessionToken
-    // @TODO: DELETE GET TOKEN RETURNING HARD-CODED TOKEN
-    // } else return 'MY^PR3TTYP0NY'
   } else return ''
 }
 
